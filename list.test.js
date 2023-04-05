a07110392539819c85a4b6e4cfffe6a2968abb94
touredri <dcompte223@gmail.com>
2023-04-05 18:02:27 +0100

    Make it AAA

--- list.test.js
+++ list.test.js
@@ -2,17 +2,27 @@ import List from './src/list.js';
 
 jest.mock('./src/list.js');
 
-describe('test remove and add list function', () => {
-  document.body.innerHTML = `
-    <input type="text" id="newtask">
-    <ul class="list-container"></ul>
-  `;
+describe('List Functions', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <input type="text" id="newtask">
+      <ul class="list-container"></ul>
+    `;
+    const task = document.querySelector('#newtask');
+    task.value = 'new task';
+  });
 
-  const task = document.querySelector('#newtask');
-  task.value = 'new task';
+  afterEach(() => {
+    List.listArray = [];
+  });
 
-  test('add list', () => {
+  test('should add a new list to List.listArray', () => {
+    // Arrange
+
+    // Act
     List.add();
+
+    // Assert
     expect(List.listArray).toEqual([
       {
         description: 'new task',
@@ -22,8 +32,14 @@ describe('test remove and add list function', () => {
     ]);
   });
 
-  test('remove list', () => {
+  test('should remove the list from List.listArray', () => {
+    // Arrange
+    List.add();
+
+    // Act
     List.remove(1);
+
+    // Assert
     expect(List.listArray).toEqual([]);
   });
 });
